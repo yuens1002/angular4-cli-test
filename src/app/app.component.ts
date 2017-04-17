c528568edb93e26ceb526d10061b0155de047830
Sunny Yuen <syuen@ad.dealeron.com>
2017-04-17 11:32:16 -0400

    proj init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works hahahahah!';
+  name = [1, 2, 3, 4, 5];
+
 }
